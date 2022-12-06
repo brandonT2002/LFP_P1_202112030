1c0422ffc88bd23f9a960d1457d1dfd82d6e85b2
brandonT2002 <3060571900304@ingenieria.usac.edu.gt>
2022-12-06 16:26:36 -0600

    adding automaton

--- LexicalAnalyzer.py
+++ LexicalAnalyzer.py
@@ -13,6 +13,9 @@ class LexicalAnalyzer:
 
     def addError(self,character):
         self.errors.append(Error(f'caracter sin reconocer - {character}',self.line,self.column))
+        self.status = 0
+        self.column += 1
+        self.buffer += ''
 
     def addToken(self,type,buffer):
         self.tokens.append(Token(type,buffer,self.line,self.column))
@@ -32,3 +35,73 @@ class LexicalAnalyzer:
         print('\nTOKENS')
         x = Prettytable()
         x.field_names = ['Token','Type','Line','Column']
+
+    def s0(self,character):
+        if character.isalpha():
+            self.status = 1
+            self.column += 1
+            self.buffer += character
+        elif character == ',':
+            self.status = 2
+            self.column += 1
+            self.buffer += character
+        elif character == ';':
+            self.status = 3
+            self.column += 1
+            self.buffer += character
+        elif character == '%':
+            self.status = 4
+            self.column += 1
+            self.buffer += character
+        elif character == '$':
+            self.status = 5
+            self.column += 1
+            self.buffer += character
+        elif character == '|':
+            self.status = 6
+            self.column += 1
+            self.buffer += character
+        elif character.isdigit():
+            self.status = 7
+            self.column += 1
+            self.buffer += character
+        elif character in [' ']:
+            self.column += 1
+        elif character == '\n':
+            self.line += 1
+            self.column += 1
+        elif character == '#':
+            pass
+        else:
+            self.addError(character)
+
+    def s1(self,character):
+        if character.isalpha() or character.isdigit() or character in ['-','_']:
+            self.status = 1
+            self.column += 1
+            self.buffer += character
+        else:
+            self.addToken('string',self.buffer)
+
+    def s2(self):
+        self.addToken('comma',self.buffer)
+    
+    def s3(self):
+        self.addToken('semicolon',self.buffer)
+
+    def s4(self):
+        self.addToken('percentage',self.buffer)
+
+    def s5(self):
+        self.addToken('dollar',self.buffer)
+
+    def s6(self):
+        self.addToken('or',self.buffer)
+
+    def s7(self,character):
+        if character.isdigit():
+            self.status = 7
+            self.column += 1
+            self.buffer += character
+        else:
+            self.addToken('number',self.buffer)
